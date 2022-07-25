2baabc54c481bbfb2328e280dda29b7d6bb82dea
Breno Nascimento <satsukebare23@gmail.com>
2022-07-24 23:28:57 -0300

    add backgrounds

---
+++ src/Components/AuthCard/AuthCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AuthFormComponent from '../AuthForm/AuthFormComponent';
+import { CardContainer } from './AuthCardStyle';
+
+function AuthCard() {
+  return (
+    <CardContainer>
+      <h1>dwaiopdjwaiodw</h1>
+      <AuthFormComponent />
+    </CardContainer>
+  );
+}
+
+export default AuthCard;
---
+++ src/Components/AuthCard/AuthCardStyle.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const CardContainer = styled.section`
+  display: flex;
+  width: 900px;
+  height: 500px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(255, 255, 255, 0.1000);;
+  backdrop-filter: blur(15px);
+  -webkit-backdrop-filter: blur(15px);
+`;
