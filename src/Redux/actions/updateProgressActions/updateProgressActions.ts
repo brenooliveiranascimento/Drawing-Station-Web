a26e4d9daf15f9c9f86fbad98e3ab02f68bf32fd
Breno Nascimento <satsukebare23@gmail.com>
2022-07-28 21:46:40 -0300

    add updateUserProgress actions

---
+++ src/Redux/actions/updateProgressActions/updateProgressActions.ts
@@ -0,0 +1,13 @@
+import { Dispatch } from 'react';
+
+const updateStoreProfress = (userData: string, dificulty: any, exercice: string) => ({
+  type: 'Updatadawiodw',
+  userData,
+  exercice,
+});
+
+export const updateuserProgress = (): any => {
+  return async (dispatch: Dispatch<any>, { userData, exerciceProgress }: any) => {
+
+  };
+};
