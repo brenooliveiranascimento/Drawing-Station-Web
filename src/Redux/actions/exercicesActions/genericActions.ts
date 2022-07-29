9de7f88d8338f09a4de2f37e0a6be0311f8df760
Breno Nascimento <satsukebare23@gmail.com>
2022-07-28 23:38:30 -0300

    add action nextExercice

--- src/Redux/actions/exercicesActions/genericActions.ts
+++ src/Redux/actions/exercicesActions/genericActions.ts
@@ -1,5 +1,5 @@
 import {
-  FETCH_EXERCICE_FAIL, FETCH_EXERCICE_INIT, FETCH_EXERCICE_SUCCESS, SET_NOW_EXERCICE,
+  FETCH_EXERCICE_FAIL, FETCH_EXERCICE_INIT, FETCH_EXERCICE_SUCCESS, NEXT_EXERCICE, SET_NOW_EXERCICE,
 } from '../../modules/exerciceData/exerciceDataTypes';
 
 export const updateExerciceStore = (exerciceData: any, modulesData: any): any => ({
@@ -20,4 +20,8 @@ export const updateExerciceStoreFail = (errorMessage: string) => ({
 export const changeNowExercice = (nowExercice: any) => ({
   type: SET_NOW_EXERCICE,
   payLoad: nowExercice,
+});
+
+export const nextExercice = () => ({
+  type: NEXT_EXERCICE,
 });
