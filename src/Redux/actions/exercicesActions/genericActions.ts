59a6bc9dc291b57ad48e4dc0e000ed3c69e6278c
Breno Nascimento <satsukebare23@gmail.com>
2022-07-28 23:39:31 -0300

    add prev exercice aciton

--- src/Redux/actions/exercicesActions/genericActions.ts
+++ src/Redux/actions/exercicesActions/genericActions.ts
@@ -1,5 +1,9 @@
 import {
-  FETCH_EXERCICE_FAIL, FETCH_EXERCICE_INIT, FETCH_EXERCICE_SUCCESS, NEXT_EXERCICE, SET_NOW_EXERCICE,
+  FETCH_EXERCICE_FAIL,
+  FETCH_EXERCICE_INIT,
+  FETCH_EXERCICE_SUCCESS,
+  NEXT_EXERCICE,
+  PREV_EXERCICE, SET_NOW_EXERCICE,
 } from '../../modules/exerciceData/exerciceDataTypes';
 
 export const updateExerciceStore = (exerciceData: any, modulesData: any): any => ({
@@ -24,4 +28,8 @@ export const changeNowExercice = (nowExercice: any) => ({
 
 export const nextExercice = () => ({
   type: NEXT_EXERCICE,
+});
+
+export const prevExercice = () => ({
+  type: PREV_EXERCICE,
 });
