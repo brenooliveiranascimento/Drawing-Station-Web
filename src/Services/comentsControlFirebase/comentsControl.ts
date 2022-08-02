2d107470b588e8f590d7581d5d53c3e103868f57
Breno Nascimento <satsukebare23@gmail.com>
2022-08-02 14:08:39 -0300

    create aupdateComents functiuo

--- src/Services/comentsControlFirebase/comentsControl.ts
+++ src/Services/comentsControlFirebase/comentsControl.ts
@@ -5,3 +5,7 @@ export const getAllComents = async () => {
     .collection('coments').doc('data').get();
   return commentsData.data();
 };
+
+export const updateComents = async (nowComments: any) => {
+  await firebase.firestore().collection('coments').doc('data').set(nowComments);
+};
