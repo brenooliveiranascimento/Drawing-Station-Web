d7fe15d4896ebad0bc8562a2d2de9989f569977a
Breno Nascimento <satsukebare23@gmail.com>
2022-08-03 19:25:27 -0300

    connect thunk in interface

--- src/Components/ExerciceComponents/Coments/ComentsArea.tsx
+++ src/Components/ExerciceComponents/Coments/ComentsArea.tsx
@@ -1,15 +1,23 @@
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { deletComment } from '../../../Redux/actions/exercicesActions/exerciceActions';
 import { ComentsContainer, CommentBody, UserDataComent } from './Components';
 
 function ComentsArea() {
+  const dispatch = useDispatch();
   const comments = useSelector(({ exerciceData }: any) => exerciceData.comments);
   const nowExercice = useSelector(({ exerciceData }: any) => exerciceData.nowExerciceData);
   const userInf = useSelector(({ userData }: any) => userData);
+
   const commentsFilter = () => {
     const filterComents = comments.filter((comment: any) => comment.exercice === nowExercice.name);
     return filterComents;
   };
+
+  const removeComment = (comment: any) => {
+    dispatch(deletComment(comment));
+  };
+
   useEffect(() => {
     commentsFilter();
   }, [nowExercice, comments]);
@@ -24,6 +32,7 @@ function ComentsArea() {
               <h2>{coment.nameOfCreator}</h2>
               { coment.uidOfCreator === userInf.uid && (
                 <button
+                  onClick={() => removeComment(coment)}
                   type="button"
                 >
                   Deletar
