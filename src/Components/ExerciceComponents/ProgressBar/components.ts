e5ecd4c9ea16b733f384ba1fe4d5f027e1a3bfb2
Breno Nascimento <satsukebare23@gmail.com>
2022-08-04 19:50:42 -0300

    crete bars components

---
+++ src/Components/ExerciceComponents/ProgressBar/components.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const MainBar = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 15px;
+  width: 400px;
+`;
+
+export const BackBar = styled.article`
+  width: 100%;
+  width: 15px;
+  background-color: black;
+  position: absolute;
+`;
+
+export const Colorbar = styled.article`
+  width: 40%;
+  position: absolute;
+  background-color: aqua;
+  height: 15px;
+`;
