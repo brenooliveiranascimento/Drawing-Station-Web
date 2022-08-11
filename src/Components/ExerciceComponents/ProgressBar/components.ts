b8c6023cc9eaccef304a08878164fbd11690ac44
Breno Nascimento <satsukebare23@gmail.com>
2022-08-11 10:23:29 -0300

    change progressBar background

--- src/Components/ExerciceComponents/ProgressBar/components.ts
+++ src/Components/ExerciceComponents/ProgressBar/components.ts
@@ -30,6 +30,6 @@ export const Colorbar = styled.article`
   position: absolute;
   height: 15px;
   z-index: 1;
-  background-color: #5303bc;
+  background-color: #350866;
   border-radius: 10px;
 `;
