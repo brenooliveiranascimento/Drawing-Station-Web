d994093d2c5323fc56c7698e8040de0880ba672d
Breno Nascimento <satsukebare23@gmail.com>
2022-08-13 13:13:04 -0300

    none

--- src/Components/AuthComponents/AuthForm/AuthForm.ts
+++ src/Components/AuthComponents/AuthForm/AuthForm.ts
@@ -42,7 +42,7 @@ select:-webkit-autofill,
 select:-webkit-autofill:hover,
 select:-webkit-autofill:focus {
   -webkit-text-fill-color: white;
-  -webkit-box-shadow: 0 0 0px 1000px #000 inset;
+  -webkit-box-shadow: 0 0 0px 1000px ease-in-out inset;
   transition: background-color 5000s ease-in-out 0s;
 }
 
